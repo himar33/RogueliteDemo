c9535ff8742481175a46e36c6ce267f91bb105ec
himarsus <60131783+himar33@users.noreply.github.com>
2022-11-16 11:37:51 +0100

    Not finished movement, fixed hand rotation

--- RogueliteDemo/Assets/Scripts/PlayerController.cs
+++ RogueliteDemo/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float speed;
     [SerializeField] private Transform shotPoint;
     [SerializeField] private GameObject bulletPrefab;
+
+    private Vector2 inputMovement;
+    private Rigidbody2D rb;
+
     private Transform handTransform;
     private Transform gunTransform;
     private Vector2 mousePosition;
@@ -19,6 +24,8 @@ public class PlayerController : MonoBehaviour
         handTransform = transform.GetChild(0);
         gunTransform = handTransform.GetChild(0);
 
+        rb = GetComponent<Rigidbody2D>();
+
         sr = GetComponent<SpriteRenderer>();
         handSR = handTransform.GetComponent<SpriteRenderer>();
         gunSR = gunTransform.GetComponent<SpriteRenderer>();
@@ -27,6 +34,9 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        inputMovement.x = Input.GetAxisRaw("Horizontal");
+        inputMovement.y = Input.GetAxisRaw("Vertical");
 
         if (mousePosition.x > handTransform.position.x && sr.flipX == true || mousePosition.x < handTransform.position.x && sr.flipX != true)
         {
@@ -37,11 +47,13 @@ public class PlayerController : MonoBehaviour
 
         Vector2 aimDirection = mousePosition - (Vector2)handTransform.position;
         float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
-        handTransform.GetComponent<Rigidbody2D>().rotation = aimAngle;
+        handTransform.eulerAngles = new Vector3(0, 0, aimAngle);
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Instantiate(bulletPrefab, shotPoint);
         }
+
+        rb.velocity = speed * Time.deltaTime * inputMovement;
     }
 }
