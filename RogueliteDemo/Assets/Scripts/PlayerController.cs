ff4c419a3ef1febedb42726c78abbc36dbe761fe
himarsus <60131783+himar33@users.noreply.github.com>
2022-11-17 11:50:12 +0100

    Camera shake added & player animations working well

--- RogueliteDemo/Assets/Scripts/PlayerController.cs
+++ RogueliteDemo/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EZCameraShake;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Stats")]
     [SerializeField] private float speed;
     [SerializeField] private float shootWalkSpeed;
+    [SerializeField] private float fireRate;
+
+    [Space]
+
+    [Header("References")]
     [SerializeField] private Transform shotPoint;
     [SerializeField] private GameObject bulletPrefab;
 
+    private bool canShoot = true;
     private Vector2 inputMovement;
     private Rigidbody2D rb;
-
+    private Animator anim;
     private Transform handTransform;
     private Transform gunTransform;
     private Vector2 mousePosition;
-
     private SpriteRenderer sr;
     private SpriteRenderer handSR;
     private SpriteRenderer gunSR;
@@ -26,8 +33,9 @@ public class PlayerController : MonoBehaviour
         gunTransform = handTransform.GetChild(0);
 
         rb = GetComponent<Rigidbody2D>();
-
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
         handSR = handTransform.GetComponent<SpriteRenderer>();
         gunSR = gunTransform.GetComponent<SpriteRenderer>();
     }
@@ -38,9 +46,12 @@ public class PlayerController : MonoBehaviour
 
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
+        float currSpeed = speed;
         inputMovement = new Vector2(moveX, moveY).normalized;
 
-        float currSpeed = speed;
+        Vector2 aimDirection = mousePosition - (Vector2)handTransform.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        handTransform.eulerAngles = new Vector3(0, 0, aimAngle);
 
         if (mousePosition.x > handTransform.position.x && sr.flipX == true || mousePosition.x < handTransform.position.x && sr.flipX != true)
         {
@@ -49,16 +60,39 @@ public class PlayerController : MonoBehaviour
             gunSR.flipY = !gunSR.flipY;
         }
 
-        Vector2 aimDirection = mousePosition - (Vector2)handTransform.position;
-        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
-        handTransform.eulerAngles = new Vector3(0, 0, aimAngle);
+        if (mousePosition.x > handTransform.position.x && moveX < 0 || mousePosition.x < handTransform.position.x && moveX > 0)
+        {
+            anim.SetBool("runningBack", true);
+        }
+        else
+        {
+            anim.SetBool("runningBack", false);
+        }
 
         if (Input.GetKey(KeyCode.Mouse0))
         {
             currSpeed = shootWalkSpeed;
-            Instantiate(bulletPrefab, shotPoint.position, shotPoint.rotation, GameObject.FindGameObjectWithTag("Destroyable").transform);
+            anim.speed = 0.5f;
+            if (canShoot)
+            {
+                StartCoroutine(Shoot());
+            }
+        }
+        else
+        {
+            anim.speed = 1.0f;
         }
 
         rb.velocity = currSpeed * inputMovement;
+        anim.SetFloat("velocity", inputMovement.magnitude);
+    }
+
+    private IEnumerator Shoot()
+    {
+        CameraShaker.Instance.ShakeOnce(2f, 2f, 0.15f, 1f);
+        canShoot = false;
+        Instantiate(bulletPrefab, shotPoint.position, shotPoint.rotation, GameObject.FindGameObjectWithTag("Destroyable").transform);
+        yield return new WaitForSeconds(fireRate);
+        canShoot = true;
     }
 }
